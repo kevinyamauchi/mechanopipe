620a7c048dbd7f7aacefb38b658710bed24e9ccb
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2024-11-14 13:49:11 +0100

    add constants

---
+++ src/mechanopipe/constants.py
@@ -0,0 +1,8 @@
+"""Constants used by mechanopipe."""
+
+# run directory name pattern: run_000X
+RUN_DIRECTORY_NAME_TEMPLATE = "run_{run_index:04}"
+RUN_DIRECTORY_GLOB_PATTERN = "run_[0-9]{4}"
+
+# directory and file names for preprocessing
+PREPROCESS_DIRECTORY = "preprocess"
